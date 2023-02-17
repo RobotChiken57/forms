37bb3c1b9ac62021f05014681584b6f3905b1178
RobotChiken57 <99732056+RobotChiken57@users.noreply.github.com>
2023-02-17 14:52:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".dws-form").on("click", ".tab", function() {
+    // Удаляем классы active
+    $(".dws-form").find(".active").removeClass("active");
+    // Добавляем классы active
+    $(this).addClass('active');
+    $(".tab-form").eq($(this).index()).addClass("active");
+});
